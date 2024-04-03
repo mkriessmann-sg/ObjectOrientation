260638bc3dfe001350b5a2cf7235403d76cd14dd
MARCO KRIESSMANN <kriessmann-marco@s-giken.com>
2024-04-03 14:38:49 +0900

    Add project files.

---
+++ ObjectOrientation/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ObjectOrientation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+       
+    }
+    public class Shoseki
+    {
+        private string title;
+        private string author;
+        private int copiesAvailable;
+
+
+        public void LendOut()
+        {
+
+
+        }
+
+        public void Return() { 
+        
+        }
+
+    }
+
+    public class Hon : Shoseki
+    {
+
+    }
+
+    public class Zasshi :Shoseki
+    {
+
+    }
+
+    public class Shinbunn : Shoseki
+    {
+
+    }
+}
