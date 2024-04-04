db4cdeac47ea47e132865069022e5daf1b92207b
MARCO KRIESSMANN <kriessmann-marco@s-giken.com>
2024-04-04 11:50:51 +0900

    ammended to include addtional requirements

--- ObjectOrientation/Program.cs
+++ ObjectOrientation/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ObjectOrientation
+﻿using System.Runtime.CompilerServices;
+
+namespace ObjectOrientation
 {
     internal class Program
     {
@@ -10,37 +12,63 @@
 
        
     }
-    public class Shoseki
+    abstract public class Shoseki
     {
-        private string title;
-        private string author;
-        private int copiesAvailable;
+        protected string title;
+        protected string author;
+        protected int copiesAvailable;
+   
+        abstract public void DisplayInformation();
 
 
         public void LendOut()
         {
-
-
+            this.copiesAvailable = this.copiesAvailable--;
         }
 
-        public void Return() { 
-        
+        public void Return() 
+        {
+            this.copiesAvailable = this.copiesAvailable++;
         }
 
     }
 
     public class Hon : Shoseki
     {
-
+        double price;
+        
+     public override void DisplayInformation()
+        {
+            Console.WriteLine("Title:" + this.title);
+            Console.WriteLine("Author:" + this.author);
+            Console.WriteLine("Title:" + this.copiesAvailable);
+            Console.WriteLine("Price:" + this.price);
+        }
     }
 
-    public class Zasshi :Shoseki
+    public class Zasshi : Shoseki
     {
+        string item;
 
+        public override void DisplayInformation()
+        {
+            Console.WriteLine("Title:" + this.title);
+            Console.WriteLine("Author:" + this.author);
+            Console.WriteLine("Title:" + this.copiesAvailable);
+            Console.WriteLine("Price:" + this.item);
+        }
     }
 
     public class Shinbunn : Shoseki
     {
+        DateOnly publishedday;
+        public override void DisplayInformation()
+        {
+            Console.WriteLine("Title:" + this.title);
+            Console.WriteLine("Author:" + this.author);
+            Console.WriteLine("Title:" + this.copiesAvailable);
+            Console.WriteLine("Price:" + this.publishedday);
+        }
 
     }
 }
